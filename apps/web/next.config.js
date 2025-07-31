20d18ba23817b16a3ab6b5ad73956d1dc3842d21
laurismelderis <lauris.melderis77@gmail.com>
2025-07-31 19:57:48 +0300

    Remove unnecessary pages

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -15,4 +15,13 @@ module.exports = {
     ]
     return config
   },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/login',
+        permanent: true,
+      },
+    ]
+  },
 }
